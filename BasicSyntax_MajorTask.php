c30499ffc65d8172693a7f53291a67c25d4660c7
POPOOLA Blessing O <43854383+QueenJay@users.noreply.github.com>
2020-10-16 00:24:40 -0700

    First side hustle task

---
+++ BasicSyntax_MajorTask.php
@@ -0,0 +1,26 @@
+<?php
+    //====== First major task =======
+
+    
+    /* 
+        Occupation: Student
+        Gender: Female
+        Sidehustle Username: BlessMe
+    */
+
+    //about me tag
+    echo "<h1>About Me</h1> <hr/>";
+
+    //your side hustle internship username
+    echo "<h3>BlessMe</h3>";
+
+    //tell me about yourself.
+    echo "<p>
+            Hello world! I am Popoola Blessing, a Lautech graduate and a sidehustle intern. 
+            I love lerarning (and yeah, teaching a bit!),
+            I don't know what else to write about me but you can always reachout if there's anything you'd like to know.
+            I love you always!!!!
+        
+        </p>";
+
+?>
